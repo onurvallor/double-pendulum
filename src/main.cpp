1aa3b42bc2b2a92d3c3ed504f9d0afd45c09fefc
Onur Vallor <onurvallor@gmail.com>
2026-04-26 10:30:49 -0400

    Adding motion to double pendulum using equation from MIT

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 int WIDTH{500};
 int HEIGHT{500};
 int MASS_RADIUS{10};
+float GRAVITY{1};
+float L1{250};
+float L2{250};
+float M1 = MASS_RADIUS;
+float M2 = MASS_RADIUS;
+
+float m1, m2, l1, l2, phi1, phi2, d_phi1, d_phi2, dd_phi1, dd_phi2;
 
 struct PendulumSection {
   Vector2 startPos;
@@ -12,14 +19,15 @@ struct PendulumSection {
 };
 
 void draw_pendulum(Vector2 startPos, float phi1 = (90 * DEG2RAD),
-                   float phi2 = (90 * DEG2RAD), float l = 100) {
+                   float phi2 = (90 * DEG2RAD), float l1 = 100.0f,
+                   float l2 = 100.0f) {
   PendulumSection ps1{.startPos = startPos,
-                      .endPos = {startPos.x + l * std::cosf(phi1),
-                                 startPos.y + l * std::sinf(phi1)}};
+                      .endPos = {startPos.x + l1 * std::sinf(phi1),
+                                 startPos.y + l1 * std::cosf(phi1)}};
 
   PendulumSection ps2{.startPos = {ps1.endPos},
-                      .endPos = {ps1.endPos.x + l * std::cosf(phi2),
-                                 ps1.endPos.y + l * std::sinf(phi2)}};
+                      .endPos = {ps1.endPos.x + l2 * std::sinf(phi2),
+                                 ps1.endPos.y + l2 * std::cosf(phi2)}};
 
   DrawLineEx(ps1.startPos, ps1.endPos, 5.0f, WHITE);
   DrawLineEx(ps2.startPos, ps2.endPos, 5.0f, WHITE);
@@ -28,15 +36,40 @@ void draw_pendulum(Vector2 startPos, float phi1 = (90 * DEG2RAD),
   DrawCircle(ps2.endPos.x, ps2.endPos.y, MASS_RADIUS, RED);
 }
 
-class PendulumValue {
-public:
-  PendulumValue();
+void step() {
+  dd_phi1 = (-GRAVITY * (2 * m1 + m2) * std::sinf(phi1) -
+             (m2 * GRAVITY * std::sinf(phi1 - 2 * phi2) -
+              2 * std::sinf(phi1 - phi2) * m2 *
+                  (std::pow(d_phi2, 2) * l2 +
+                   std::pow(d_phi1, 2) * l1 * std::cosf(phi1 - phi2)))) /
+            (l1 * (2 * m1 + m2 - m2 * std::cosf(2 * phi1 - 2 * phi2)));
 
-private:
-  float m_x{};
-  float m_y{};
-  float m_l{};
-};
+  dd_phi2 = (2 * std::sinf(phi1 - phi2) *
+             (std::pow(d_phi1, 2) * l1 * (m1 + m2) +
+              GRAVITY * (m1 + m2) * std::cosf(phi1) +
+              std::pow(d_phi2, 2) * l2 * m2 * std::cosf(phi1 - phi2))) /
+            (l2 * (2 * m1 + m2 - m2 * std::cosf(2 * phi1 - 2 * phi2)));
+
+  d_phi1 += dd_phi1;
+  d_phi2 += dd_phi2;
+
+  phi1 += d_phi1;
+  phi2 += d_phi2;
+}
+
+void init_variables() {
+
+  m1 = M1;
+  m2 = M2;
+  l1 = L1;
+  l2 = L2;
+
+  phi1 = -90 * DEG2RAD;
+  phi2 = 90 * DEG2RAD;
+
+  d_phi1 = 0;
+  d_phi2 = 0;
+}
 
 int main() {
 
@@ -44,11 +77,15 @@ int main() {
 
   SetTargetFPS(60);
 
+  init_variables();
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    draw_pendulum(Vector2{WIDTH / 2.0f, 0.0f});
+    step();
+
+    draw_pendulum(Vector2{WIDTH / 2.0f, 0.0f}, phi1, phi2);
 
     EndDrawing();
   }
