13ea38e4e8ba95d6de531fdc7a127373ec9d181f
Onur Vallor <onurvallor@gmail.com>
2026-04-26 01:49:08 -0400

    Created struct for sections of the pendulum, completed render of pendulum, fixed drawing order

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,26 @@
 int WIDTH{500};
 int HEIGHT{500};
 
-void draw_pendulum(Vector2 startPos, float phi1, float l) {
+struct PendulumSection {
+  Vector2 startPos;
+  Vector2 endPos;
+};
 
-  Vector2 endPos{startPos.x + l * std::cosf(phi1),
-                 startPos.y + l * std::sinf(phi1)};
-  DrawLineEx(startPos, endPos, 5.0f, WHITE);
-  DrawCircle(endPos.x, endPos.y, 10, RED);
+void draw_pendulum(Vector2 startPos, float phi1 = (90 * DEG2RAD),
+                   float phi2 = (90 * DEG2RAD), float l = 100) {
+  PendulumSection ps1{.startPos = startPos,
+                      .endPos = {startPos.x + l * std::cosf(phi1),
+                                 startPos.y + l * std::sinf(phi1)}};
+
+  PendulumSection ps2{.startPos = {ps1.endPos},
+                      .endPos = {ps1.endPos.x + l * std::cosf(phi2),
+                                 ps1.endPos.y + l * std::sinf(phi2)}};
+
+  DrawLineEx(ps1.startPos, ps1.endPos, 5.0f, WHITE);
+  DrawLineEx(ps2.startPos, ps2.endPos, 5.0f, WHITE);
+
+  DrawCircle(ps1.endPos.x, ps1.endPos.y, 10, RED);
+  DrawCircle(ps2.endPos.x, ps2.endPos.y, 10, RED);
 }
 
 int main() {
@@ -21,7 +35,7 @@ int main() {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    draw_pendulum(Vector2{WIDTH / 2.0f, 0.0f}, 90 * DEG2RAD, 100);
+    draw_pendulum(Vector2{WIDTH / 2.0f, 0.0f});
 
     EndDrawing();
   }
